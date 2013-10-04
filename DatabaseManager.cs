7a3480df8fe6fad186686932192d30ebcdbe8fd8
Zach Huston <zahuston@umich.edu>
2013-10-04 18:18:15 -0400

    Added Mongo drivers

--- DatabaseManager.cs
+++ DatabaseManager.cs
@@ -1,8 +1,22 @@
 ﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+using MongoDB.Driver.Builders;
+
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Bson.Serialization.Options;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver.Wrappers;
 
 public class DatabaseManager
 {
 	public DatabaseManager()
 	{
+            
 	}
 }
